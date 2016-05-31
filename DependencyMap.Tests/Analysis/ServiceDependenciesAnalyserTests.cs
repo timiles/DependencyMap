6f8fb02c7dd3a1f85e046b8917f7271a657938b8
Tim Iles <tim@timiles.com>
2016-06-01 00:07:12 +0100

    test a simple dependency

--- DependencyMap.Tests/Analysis/ServiceDependenciesAnalyserTests.cs
+++ DependencyMap.Tests/Analysis/ServiceDependenciesAnalyserTests.cs
@@ -1,5 +1,8 @@
 using System;
 using DependencyMap.Analysis;
+using DependencyMap.Models;
+using FluentAssertions;
+using NuGet;
 using NUnit.Framework;
 
 namespace DependencyMap.Tests.Analysis
@@ -11,6 +14,29 @@ namespace DependencyMap.Tests.Analysis
         public void NullList_ShouldThrowException()
         {
             Assert.Throws<ArgumentNullException>(() => new ServiceDependenciesAnalyser(null));
+        }
+
+        [Test]
+        public void SimpleDependency_GroupedByService_ShouldReturnServicesAsExpected()
+        {
+            var myServiceDependency = new ServiceDependency
+            {
+                ServiceId = "MyService",
+                DependencyId = "MyDependency",
+                DependencyVersion = new SemanticVersion(1, 0, 0, 0)
+            };
+            var analyser = new ServiceDependenciesAnalyser(new[] {myServiceDependency});
+            var service = analyser.GroupByService();
+
+            service.Keys.Count.Should().Be(1);
+            service.Keys.Should().Contain(myServiceDependency.ServiceId);
+            service[myServiceDependency.ServiceId].Length.Should().Be(1);
+
+            var dependencyStaleness = service[myServiceDependency.ServiceId][0];
+            dependencyStaleness.DependencyId.Should().Be(myServiceDependency.DependencyId);
+            dependencyStaleness.Version.Should().Be(myServiceDependency.DependencyVersion);
+            dependencyStaleness.LatestKnownVersion.Should().Be(myServiceDependency.DependencyVersion);
+            dependencyStaleness.StalenessRating.Should().Be(0);
         }
     }
 }
